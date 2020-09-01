4f97cf6d95b9e4e2f5b8cc2af1aa373d32976807
Jakub <jakubcekala1@gmail.com>
2020-09-01 18:46:50 +0200

    Create config.properties file with configuration values

--- src/main/java/com/qa/BaseTest.java
+++ src/main/java/com/qa/BaseTest.java
@@ -7,25 +7,39 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
     protected AppiumDriver driver;
+    protected Properties props;
+    InputStream inputStream;
 
     @BeforeTest
     public void beforeTest() {
 
         try {
+
+            props = new Properties();
+            String propsFileName = "config.properties";
+
+            inputStream = getClass().getClassLoader().getResourceAsStream(propsFileName);
+            props.load(inputStream);
+
             DesiredCapabilities caps = new DesiredCapabilities();
             caps.setCapability("platformName", "Android");
-            caps.setCapability("platformName", "9.0");
-            caps.setCapability("platformName", "device name");
-            caps.setCapability("platformName", "UiAutomator2");
-            caps.setCapability("platformName", "com.swaglabsmobileapp");
-            caps.setCapability("platformName", "com.swaglabmobileapp.SplashActivity");
+            caps.setCapability("platformVersion", "9.0");
+            caps.setCapability("deviceName", "device name");
+            caps.setCapability("automationName", props.getProperty("androidAutomationName"));
+            caps.setCapability("appPackage", props.getProperty("androidAppPackage"));
+            caps.setCapability("appActivity", props.getProperty("androidAppActivity"));
+            URL appURL = getClass().getClassLoader().getResource("androidAppLocation");
+            caps.setCapability("app", appURL);
 
             URL url = new URL("http://0.0.0.0:4723/wd/hub");
             driver = new AndroidDriver(url, caps);
@@ -33,6 +47,8 @@ public class BaseTest {
             String sessionId = driver.getSessionId().toString();
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
