229bc487cbcd61a8b249dda2b2fd9d0fcc870e82
Jakub <jakubcekala1@gmail.com>
2020-09-01 23:12:30 +0200

    Add json data file

--- src/test/java/com/qa/tests/LoginTests.java
+++ src/test/java/com/qa/tests/LoginTests.java
@@ -3,19 +3,36 @@ package com.qa.tests;
 import com.qa.BaseTest;
 import com.qa.pages.LoginPage;
 import com.qa.pages.ProductsPage;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 
 public class LoginTests extends BaseTest {
 
     LoginPage loginPage;
     ProductsPage productsPage;
+    InputStream dataIS;
+    JSONObject loginUsers;
 
     @BeforeClass
-    public void beforeClass() {
-
+    public void beforeClass() throws Exception {
+        try {
+            String dataFilename = "data/loginUsers.json";
+            dataIS = getClass().getClassLoader().getResourceAsStream(dataFilename);
+            JSONTokener tokener = new JSONTokener(dataIS);
+            loginUsers = new JSONObject(tokener);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (dataIS != null) {
+                dataIS.close();
+            }
+        }
     }
 
     @BeforeMethod
@@ -26,8 +43,8 @@ public class LoginTests extends BaseTest {
 
     @Test
     public void invalidUsername() {
-        loginPage.enterUsername("invalidUsername");
-        loginPage.enterPassword("secret_sauce");
+        loginPage.enterUsername(loginUsers.getJSONObject("invalidUser").getString("username"));
+        loginPage.enterPassword(loginUsers.getJSONObject("invalidUser").getString("password"));
         loginPage.pressLoginButton();
 
         String actualErrorText = loginPage.getErrorText();
@@ -39,8 +56,8 @@ public class LoginTests extends BaseTest {
 
     @Test
     public void invalidPassword() {
-        loginPage.enterUsername("standard_user");
-        loginPage.enterPassword("invalidPassword");
+        loginPage.enterUsername(loginUsers.getJSONObject("invalidPassword").getString("username"));
+        loginPage.enterPassword(loginUsers.getJSONObject("invalidPassword").getString("password"));
         loginPage.pressLoginButton();
 
         String actualErrorText = loginPage.getErrorText();
@@ -52,8 +69,8 @@ public class LoginTests extends BaseTest {
 
     @Test
     public void successfulLogin() {
-        loginPage.enterUsername("standard_user");
-        loginPage.enterPassword("secret_sauce");
+        loginPage.enterUsername(loginUsers.getJSONObject("validUser").getString("username"));
+        loginPage.enterPassword(loginUsers.getJSONObject("validUser").getString("password"));
         productsPage = loginPage.pressLoginButton();
 
         String actualTitle = productsPage.getTitle();
